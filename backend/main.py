6aa84611ebc7a106ba1f428c8e90fda0bfa2a0f0
Luke Holland <lukeholland15500@gmail.com>
2024-05-06 14:52:10 +0100

    Added cors for localhost and my domains to backend

--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,32 @@
 from contextlib import asynccontextmanager
 from fastapi import Depends, FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from routers import recipes, users
 from database import get_session
 
+# Potentially don't include local host when running in PROD environment
+origins = [
+    "http://localhost",
+    "https://localhost",
+    "http://localhost:5173",
+    "https://localhost:5173",
+    "http://lukeholland.dev",
+    "https://lukeholland.dev",
+    "http://recipes.lukeholland.dev",
+    "https://recipes.lukeholland.dev",
+]
 
 app = FastAPI()
 app.include_router(recipes.router, dependencies=[Depends(get_session)])
 app.include_router(users.router, dependencies=[Depends(get_session)])
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/ping")
