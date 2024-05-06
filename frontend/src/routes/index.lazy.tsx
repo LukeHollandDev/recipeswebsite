87ae9b8f5c9babd22edde66c4ceca84093e114ea
Luke Holland <lukeholland15500@gmail.com>
2024-05-06 21:23:53 +0100

    Added hero to the home page

--- frontend/src/routes/index.lazy.tsx
+++ frontend/src/routes/index.lazy.tsx
@@ -6,8 +6,27 @@ export const Route = createLazyFileRoute("/")({
 
 function Index() {
   return (
-    <div className="p-4">
-      <h3>Home page will go here</h3>
-    </div>
+    <>
+      <div className="rounded bg-base-200 text-justify py-6 mb-6">
+        <div className="max-w-xl mx-auto">
+          <h1 className="text-4xl font-bold text-center">Hello Freshed 2</h1>
+          <p className="py-4">
+            Welcome to <strong>Hello Freshed 2</strong>. This is a recipe
+            website which brings together recipes from a variety of sources and
+            allows you to save them to the your account. You can favourite
+            recipes and build a menu of recipes you want to cook for the week!
+          </p>
+          <div className="flex gap-2">
+            <button className="btn btn-primary grow">
+              Have an account? Login!
+            </button>
+            <button className="btn btn-secondary grow">
+              Create a new account!
+            </button>
+          </div>
+        </div>
+      </div>
+      recipes go here!
+    </>
   );
 }
